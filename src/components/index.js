828d711ebb614e15b983d86e25f5cbf7e3cde623
salimovone <salimovone@DESKTOP-02FPF0R>
2025-12-21 11:16:49 +0500

    nuke

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BottomBar } from './BottomBar/index.jsx'
-export { default as LocationCard } from './LocationCard/index.jsx'
-export { default as TableCard } from './TableCard/index.jsx'
-export { default as DateRangePicker } from './DateRangePicker/index.jsx'
--- src/views/Orders/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as LocationFilter } from "./LocationFilter";
-export { default as UserFilter } from "./UserFilter";
